8efc371cae4adbf3ac37e25ecfcce4f9d8e2107d
Ahmet Çelik <ahmet.celik2605@outlook.com>
2025-03-29 12:25:37 +0300

    Add project files.

---
+++ src/FeatureRequestPortal.Application/FeatureRequestPortalApplicationAutoMapperProfile.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+
+namespace FeatureRequestPortal;
+
+public class FeatureRequestPortalApplicationAutoMapperProfile : Profile
+{
+    public FeatureRequestPortalApplicationAutoMapperProfile()
+    {
+        /* You can configure your AutoMapper mapping configuration here.
+         * Alternatively, you can split your mapping configurations
+         * into multiple profile classes for a better organization. */
+    }
+}
---
+++ src/FeatureRequestPortal.Web/FeatureRequestPortalWebAutoMapperProfile.cs
@@ -0,0 +1,11 @@
+using AutoMapper;
+
+namespace FeatureRequestPortal.Web;
+
+public class FeatureRequestPortalWebAutoMapperProfile : Profile
+{
+    public FeatureRequestPortalWebAutoMapperProfile()
+    {
+        //Define your object mappings here, for the Web project
+    }
+}
